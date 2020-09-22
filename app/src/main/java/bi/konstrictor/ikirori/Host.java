cd567c149e3c2ab9f770bf8143d352dd6ca3383e
konstrictor <jonkur96@gmail.com>
2020-09-22 23:36:55 +0200

    added MainActivity and Host

---
+++ app/src/main/java/bi/konstrictor/ikirori/Host.java
@@ -0,0 +1,81 @@
+package bi.konstrictor.ikirori;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+/**
+ * Created by KonstrIctor on 24/03/2020.
+ */
+
+class Host {
+//    public static final String URL = "https://api.rcretraining.com";
+    public static final String URL = "http://10.0.2.2:8000";
+//    public static final String URL = "http://192.168.8.101:8000";
+
+    public static void refreshToken(final Activity activity){
+
+        final SharedPreferences sharedPreferences = activity.getSharedPreferences("user_login", Context.MODE_PRIVATE);
+
+        String refresh = sharedPreferences.getString("refresh", "");
+        String token = sharedPreferences.getString("token", "");
+
+        if (refresh.isEmpty()) {
+            activity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(activity, "Log in first", Toast.LENGTH_SHORT).show();
+                }
+            });
+            return;
+        }
+
+        String json = "{\"refresh\":\""+refresh+"\"}";
+        RequestBody body = RequestBody.create(
+                MediaType.parse("application/json; charset=utf-8"), json);
+
+        OkHttpClient client = new OkHttpClient();
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Host.URL+"/refresh/").newBuilder();
+
+        String url = urlBuilder.build().toString();
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                String json = response.body().string();
+                try {
+                    JSONObject jsonObject = new  JSONObject(json);
+                    String token = jsonObject.getString("access");
+                    SharedPreferences.Editor session = sharedPreferences.edit();
+                    session.putString("token", token);
+                    session.commit();
+
+                } catch (Exception e) {
+                    final String message = e.getMessage();
+                }
+            }
+        });
+    }
+}
+
---
+++ app/src/main/java/bi/konstrictor/ikirori/MainActivity.java
@@ -0,0 +1,38 @@
+package bi.konstrictor.ikirori;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Menu menu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu( Menu menu) {
+        getMenuInflater().inflate( R.menu.user_menu, menu);
+        this.menu = menu;
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.action_logout){
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
